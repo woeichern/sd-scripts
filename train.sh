cd42b7ef7d9ff5c034570eef6fee8fa358dd2e04
WilliamGlossika <william@glossika.com>
2023-08-01 14:42:58 +0800

    update

---
+++ train.sh
@@ -0,0 +1,4 @@
+python ./train_network.py --pretrained_model_name_or_path="/Users/weichenlin/Stable Diffusion/models/Stable-diffusion/sexSexyEasternExperienceV2_sexV20.safetensors" \
+--dataset_config=config.toml \
+--network_module=networks.lora \
+--output_dir=./output
