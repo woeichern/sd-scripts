550888b90b085aabe1a8e2f46a33758f2ae5b0a4
WilliamGlossika <william@glossika.com>
2023-08-02 08:38:14 +0800

    config

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 python ./train_network.py --pretrained_model_name_or_path="/Users/weichenlin/Stable Diffusion/models/Stable-diffusion/sexSexyEasternExperienceV2_sexV20.safetensors" \
 --dataset_config=config.toml \
 --network_module=networks.lora \
---output_dir=./output
+--max_train_steps=2000 \
+--output_dir=./output \
+--output_name=william2000_5
