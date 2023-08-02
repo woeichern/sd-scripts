758c702cf7cb5ce7f23be8bfae754d3e1869ab79
William <woeichern@gmail.com>
2023-08-02 13:26:31 +0800

    update

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
-python ./train_network.py --pretrained_model_name_or_path="/Users/weichenlin/Stable Diffusion/models/Stable-diffusion/sexSexyEasternExperienceV2_sexV20.safetensors" \
+python3
+python3 ./train_network.py --pretrained_model_name_or_path="models/sexSexyEasternExperienceV2_sexV20.safetensors" \
 --dataset_config=config.toml \
 --network_module=networks.lora \
 --max_train_steps=2000 \
---output_dir=./output \
+--output_dir=output \
 --output_name=william2000_5
