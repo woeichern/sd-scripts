a218b576071da907497092576cf3f7a758b87c6e
WilliamGlossika <william@glossika.com>
2023-08-04 22:54:21 +0800

    update

--- train.sh
+++ train.sh
@@ -1,9 +1,12 @@
 virtualenv -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 ./train_network.py --pretrained_model_name_or_path="models/sexSexyEasternExperienceV2_sexV20.safetensors" \
+python3 ./train_network.py --pretrained_model_name_or_path="models/beautifulRealistic_v60.safetensors" \
 --dataset_config=config.toml \
 --network_module=networks.lora \
---max_train_steps=2000 \
+--save_every_n_epoch \
+--lowram \
+--men_eff_attn \
+--max_train_steps=4000 \
 --output_dir=output \
---output_name=william2000_5
+--output_name=4william_4000_5
